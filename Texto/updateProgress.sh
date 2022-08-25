b0b917411e98b421ab9bf128a00fbd3d61f675cf
JoaoReis <joaovreis015@gmail.com>
2022-08-25 18:00:44 -0300

    TCC Inicial

---
+++ Texto/updateProgress.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# If you want to run this as a cron job, on OSX you need to add the following to your PATH
+# Not on OSX, you'll need to find the corresponding directories
+# export PATH=/usr/local/bin:/Library/TeX/texbin/:$PATH
+
+echo "Updating progress"
+TEX_DOC='Texto.tex'
+DOCUMENT='Texto.pdf'
+PROGRESSFILE='progress.csv'
+
+# Setup CSV if it doesn't exist
+if [ ! -f ${PROGRESSFILE} ]; then
+    echo "timestamp,wordcount,pagecount" >> ${PROGRESSFILE}
+fi
+
+
+WORDCOUNT=`texcount -sum -total -merge {$TEX_DOC} | grep "Sum count:" | tr -d "Sum count: "`
+# Use this line in OSX
+#PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
+# Use this line in Linux
+ PAGECOUNT=`pdfinfo ${DOCUMENT} | grep Pages | sed 's/[^0-9]*//'`
+
+echo `date '+%Y-%m-%d %H:%M:%S'`,$WORDCOUNT,$PAGECOUNT >> $PROGRESSFILE
+echo "Done! Page count ${PAGECOUNT}, word count ${WORDCOUNT}. Written to ${PROGRESSFILE}"
+
+
